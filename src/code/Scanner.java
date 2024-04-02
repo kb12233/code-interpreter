cc70315554edb532cf5efcd6b86c94990332b956
kb12233 <kaybeeocfemia@gmail.com>
2024-04-02 21:58:19 +0800

    Updated Scanner and TokenType

--- src/code/Scanner.java
+++ src/code/Scanner.java
@@ -14,6 +14,26 @@ class Scanner {
     private int current = 0;
     private int line = 1;
 
+    private static final Map<String, TokenType> keywords;
+
+    static {
+        keywords = new HashMap<>();
+        keywords.put("INT",    INT);
+        keywords.put("CHAR",   CHAR);
+        keywords.put("STRING", STRING);
+        keywords.put("BOOL",   BOOL);
+        keywords.put("FLOAT",  FLOAT);
+        keywords.put("AND",    AND);
+        keywords.put("OR",     OR);
+        keywords.put("NOT",    NOT);
+        keywords.put("IF",     IF);
+        keywords.put("ELSE",   ELSE);
+        keywords.put("WHILE",  WHILE);
+        keywords.put("TRUE",   TRUE);
+        keywords.put("FALSE",  FALSE);
+        keywords.put("PRINT",  PRINT);
+    }
+
     Scanner(String source) {
         this.source = source;
     }
@@ -44,9 +64,128 @@ class Scanner {
             case '*': addToken(STAR); break;
             case '/': addToken(SLASH); break;
             case '%': addToken(PERCENT); break;
-            case '#': addToken(SHARP); break;
             case '&': addToken(AMPERSAND); break;
+            case '=':
+                addToken(match('=') ? EQUAL_EQUAL : EQUAL);
+                break;
+            case '>':
+                addToken(match('=') ? GREATER_EQUAL : GREATER);
+                break;
+            case '<':
+                //addToken(match('=') ? LESS_EQUAL : LESS);
+                if (match('=')) {
+                    addToken(LESS_EQUAL);
+                } else if (match('>')) {
+                    addToken(LESS_GREATER);
+                } else {
+                    addToken(LESS);
+                }
+                break;
+            case '#':
+                // A comment goes until the end of the line.
+                while (peek() != '\n' && !isAtEnd()) advance();
+                break;
+            case ' ':
+            case '\r':
+            case '\t':
+                // Ignore whitespace.
+                break;
+
+            case '\n':
+                line++;
+                break;
+            case '"': string(); break;
+            default:
+                if (isDigit(c)) {
+                    number();
+                } else if (isAlpha(c)) {
+                    identifier();
+                } else {
+                    Code.error(line, "Unexpected character.");
+                }
+                break;
         }
+    }
+
+    private void identifier() {
+        while (isAlphaNumeric(peek())) {
+            advance();
+        }
+
+        String text = source.substring(start, current);
+        TokenType type = keywords.get(text);
+        if (type == null) type = IDENTIFIER;
+        addToken(type);
+    }
+
+    private void number() {
+        while (isDigit(peek())) advance();
+
+        // Look for a fractional part.
+        boolean isFloat = false;
+        if (peek() == '.' && isDigit(peekNext())) {
+            // Consume the "."
+            advance();
+
+            while (isDigit(peek())) advance();
+        }
+
+        if (isFloat) {
+            addToken(FLOAT_LITERAL, Double.parseDouble(source.substring(start, current)));
+        } else {
+            addToken(INT_LITERAL, Integer.parseInt(source.substring(start, current)));
+        }
+    }
+
+    private void string() {
+        while (peek() != '"' && !isAtEnd()) {
+            if (peek() == '\n') line++;
+            advance();
+        }
+
+        if (isAtEnd()) {
+            Code.error(line, "Unterminated string.");
+            return;
+        }
+
+        // The closing ".
+        advance();
+
+        // Trim the surrounding quotes.
+        String value = source.substring(start + 1, current - 1);
+        addToken(STRING_LITERAL, value);
+    }
+
+    private boolean match(char expected) {
+        if (isAtEnd()) return false;
+        if (source.charAt(current) != expected) return false;
+
+        current++;
+        return true;
+    }
+
+    private char peek() {
+        if (isAtEnd()) return '\0';
+        return source.charAt(current);
+    }
+
+    private char peekNext() {
+        if (current + 1 >= source.length()) return '\0';
+        return source.charAt(current + 1);
+    }
+
+    private boolean isAlpha(char c) {
+        return (c >= 'a' && c <= 'z') ||
+                (c >= 'A' && c <= 'Z') ||
+                c == '_';
+    }
+
+    private boolean isAlphaNumeric(char c) {
+        return isAlpha(c) || isDigit(c);
+    }
+
+    private boolean isDigit(char c) {
+        return c >= '0' && c <= '9';
     }
 
     private boolean isAtEnd() {

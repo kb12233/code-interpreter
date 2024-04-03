5cc137602471c7851c4628fd7f0f0607e8694ba4
kb12233 <kaybeeocfemia@gmail.com>
2024-04-03 23:35:14 +0800

    Added a class for expressions

--- src/code/Token.java
+++ src/code/Token.java
@@ -14,6 +14,6 @@ class Token {
     }
 
     public String toString() {
-        return type + " " + lexeme + " " + literal;
+        return "{TokenType: " + type + ", Lexeme: " + lexeme + ", Literal: " + literal + "}";
     }
 }
